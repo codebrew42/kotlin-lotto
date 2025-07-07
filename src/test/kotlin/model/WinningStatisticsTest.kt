a8224e57d6b0b712f7510b0722eb8f84db8ff957
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-07 10:09:45 +0200

    style: replace kotlin.test.* with org.uunit.jupiter.api.* of the test codes

--- src/test/kotlin/model/WinningStatisticsTest.kt
+++ src/test/kotlin/model/WinningStatisticsTest.kt
@@ -1,7 +1,7 @@
 package model
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 class WinningStatisticsTest {
     @Test

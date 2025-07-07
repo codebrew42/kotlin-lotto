d0b64a3e5d0eaf1786b31899426e907fafddd4f6
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-07 12:42:45 +0200

    style: organize messages for better readability

--- src/main/kotlin/view/ErrorMessages.kt
+++ src/main/kotlin/view/ErrorMessages.kt
@@ -1,11 +1,13 @@
 package view
 
 enum class ErrorMessages(val message: String) {
-    ERROR_INVALID_UNIT("[ERROR] purchase amount must be divisible by 1000"),
-    INVALID_INPUT("[ERROR] The input is invalid!"),
-    RETRY_INPUT("[ERROR] Enter the input again!"),
-    INVALID_DIGITS("[ERROR] Please enter only numbers."),
-    INVALID_BONUS_RANGE("[ERROR] The bonus number must be between 1 and 45."),
-    DUPLICATE_NUMBER("[ERROR] Duplicate numbers are not allowed."),
+    INPUT_EMPTY("[ERROR] The input is empty!"),
+    INPUT_TOO_MANY_ATTEMPT("[ERROR] You reached maximum number of attempts."),
+    INPUT_INVALID_DIGITS("[ERROR] Please enter valid numbers."),
+
+    PURCHASE_AMOUNT_INVALID_UNIT("[ERROR] purchase amount must be divisible by 1000"),
+    BONUS_NUMBER_OUT_OF_RANGE("[ERROR] The bonus number must be between 1 and 45."),
+    NUMBER_DUPLICATE("[ERROR] Duplicate numbers are not allowed."),
+
     INVALID_TICKET_LENGTH("[ERROR] Please enter exactly 6 numbers."),
 }
--- src/main/kotlin/view/PromptMessages.kt
+++ src/main/kotlin/view/PromptMessages.kt
@@ -1,7 +1,7 @@
 package view
 
 enum class PromptMessages(val message: String) {
-    GET_PURCHASE_AMOUNT("Please enter the purchase amount.\n"),
-    GET_WINNING_NUMBERS("Please enter last week's winning numbers.\n"),
-    GET_BONUS_NUMBER("Please enter last week's bonus number.\n"),
+    GET_PURCHASE_AMOUNT("Please enter the purchase amount."),
+    GET_WINNING_NUMBERS("Please enter last week's winning numbers."),
+    GET_BONUS_NUMBER("Please enter last week's bonus number."),
 }

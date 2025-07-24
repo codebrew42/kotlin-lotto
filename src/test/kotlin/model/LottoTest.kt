fabbc2e8f7b7ea79ad6537aa4d8012b431753c6c
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-24 17:14:15 +0200

    fix: change number of manual tickets from 'var' to parameter

--- src/test/kotlin/model/LottoTest.kt
+++ src/test/kotlin/model/LottoTest.kt
@@ -9,21 +9,21 @@ class LottoTest {
     @Test
     fun `should throw exception - invalid purchase amount not divisible by unit`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(10)
+            Lotto(10, 0)
         }
     }
 
     @Test
     fun `should throw exception - invalid purchase amount negative`() {
         assertThrows<IllegalArgumentException> {
-            Lotto(-10)
+            Lotto(-10, 0)
         }
     }
 
     @Test
     fun `should not throw exception - valid purchase amount equal to unit`() {
         assertDoesNotThrow {
-            val lotto = Lotto(1000)
+            val lotto = Lotto(1000, 0)
             assertEquals(1, lotto.numberOfTotalTickets)
         }
     }
@@ -31,14 +31,14 @@ class LottoTest {
     @Test
     fun `should not throw exception - valid purchase amount above unit`() {
         assertDoesNotThrow {
-            val lotto = Lotto(15000)
+            val lotto = Lotto(15000, 0)
             assertEquals(15, lotto.numberOfTotalTickets)
         }
     }
 
     @Test
     fun `generateAutomaticTickets - should create correct number of tickets`() {
-        val lotto = Lotto(5000)
+        val lotto = Lotto(5000, 0)
         assertDoesNotThrow {
             lotto.generateAutomaticTickets()
         }
@@ -47,7 +47,7 @@ class LottoTest {
 
     @Test
     fun `number should be properly saved - check manual creation of lotto tickets`() {
-        val lotto = Lotto(2000)
+        val lotto = Lotto(2000, 2)
         val ticket1 = Ticket(listOf(1, 2, 3, 4, 5, 6))
         val ticket2 = Ticket(listOf(7, 8, 9, 10, 11, 12))
         lotto.tickets.add(ticket1)
@@ -59,7 +59,7 @@ class LottoTest {
 
     @Test
     fun `manually added tickets are used for game logic`() {
-        val lotto = Lotto(1000)
+        val lotto = Lotto(1000, 1)
         val myTicket = Ticket(listOf(1, 2, 3, 10, 20, 30))
         lotto.tickets.add(myTicket)
         assertEquals(1, lotto.tickets.size)

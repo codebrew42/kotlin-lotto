7092af91ecf5d0fb5e4fc6c48cd1c802d0fa503c
Jin Park <jieun.park.e@deliveryhero.com>
2025-07-24 16:06:33 +0200

    fix(Lotto.kt): add proper validation of number of manual tickets

--- src/main/kotlin/model/Lotto.kt
+++ src/main/kotlin/model/Lotto.kt
@@ -4,6 +4,11 @@ import view.ErrorMessages
 class Lotto(val purchaseAmount: Int) {
     val numberOfTotalTickets: Int
     var numberOfManualTickets: Int = 0
+        set(value) {
+            require(value >= 0) { MANUAL_TICKET_INVALID_NOT_POSITIVE }
+            require(value <= numberOfTotalTickets) { MANUAL_TICKET_INVALID_TOO_LARGE }
+            field = value
+        }
     var numberOfAutomaticTickets: Int = 0
     val tickets = mutableListOf<Ticket>()
 
@@ -35,5 +40,7 @@ class Lotto(val purchaseAmount: Int) {
         const val TICKET_LENGTH = 6
         const val TICKET_NUMBER_MIN = 1
         const val TICKET_NUMBER_MAX = 45
+        const val MANUAL_TICKET_INVALID_NOT_POSITIVE = "[ERROR] Manual ticket amount must be positive or zero"
+        const val MANUAL_TICKET_INVALID_TOO_LARGE = "[ERROR] Manual tickets cannot exceed total tickets"
     }
 }
